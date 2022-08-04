b03f472be8e61fb74ce17bb8e1b3d91fce648850
hoshitannn <97015829+hoshitannn@users.noreply.github.com>
2022-08-04 22:04:27 +0900

    Add files via upload

--- cardreader/reader.js
+++ cardreader/reader.js
@@ -112,7 +112,7 @@ function scanCardLoop() {
  
     req.open('POST', "https://forwarrd.tk/cgi-bin/post.cgi");
     req.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
-    req.send("image=" + base64.replace(";", ""));
+    req.send("image=" + base64.replace(";", "").replace("+", "＋"));
     
     // connection.send("start-img");
     // for (const r of result) {
